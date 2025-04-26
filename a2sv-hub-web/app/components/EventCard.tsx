99b2f312502ceba45c0111e7bbe7adc49423961e
melki <melkishitesfayeangassa@gmail.com>
2025-04-26 14:59:07 +0300

    Fix: Redirect the homepage logic from home directory to dashboard root

--- a2sv-hub-web/app/components/EventCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EventCard = () => {
-  return (
-    <div className='shadow-md rounded-2xl text-sm font-bold text-center place-content-center w-[40%]'> NO NEW EVENT </div>
-  )
-}
-
-export default EventCard
