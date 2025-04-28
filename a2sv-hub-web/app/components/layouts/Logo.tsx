00d3f6d16300cba970153f69fddba4acfcc00fa6
melki <melkishitesfayeangassa@gmail.com>
2025-04-28 16:04:04 +0300

    Implement NavBar

---
+++ a2sv-hub-web/app/components/layouts/Logo.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <main className="flex items-center justify-center  bg-white">
+      <div className={cn("flex items-center space-x-2")}>
+        <div className="bg-gradient-to-br from-green-500 to-green-600 rounded-[12px] p-2 w-10 h-10">
+         <Image src="/africa.png" alt="User profile" width={45} height={45} className="rounded-full" />
+        </div>
+
+        <div className="flex text-3xl font-semibold text-green-600">HUB</div>
+      </div>
+    </main>
+  );
+};
+
